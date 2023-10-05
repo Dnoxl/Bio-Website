33419972153c7512cf97467f43ae7a1bd03e8a9b
Dnoxl <ifbimgamer@gmail.com>
2023-10-05 22:52:50 +0200

    added simple project list

--- static/navbar.js
+++ static/navbar.js
@@ -35,10 +35,33 @@ function resize() {
     }
     if (window.innerWidth < 480) {
         nav = document.getElementById("myNav")
+        var x = document.getElementById("proj-list");
+        if (x.style.display === "flex") {
+            x.style.display = "none";
+        }
         if (nav.classList.contains("open")) {
             nav.classList.remove("open");
         }
         currentRotation = "0deg";
         document.getElementById("navLogo").style.transform = "rotate(0deg)";
     }
+};
+function toggleProj() {
+    var x = document.getElementById("proj-list");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+        setProjListPosition()
+    } else {
+        x.style.display = "none";
+    }
 }
+function setProjListPosition() {
+    var projButton = document.getElementById("proj-button");
+    var projList = document.getElementById("proj-list");
+    var buttonRect = projButton.getBoundingClientRect();
+    var listWidth = projList.getBoundingClientRect().width/2;
+    var centerX = buttonRect.left + buttonRect.width / 2;
+    projList.style.left = (centerX - listWidth) + "px";
+    console.log(listWidth)
+}
+window.addEventListener("resize", setProjListPosition);
--- views.py
+++ views.py
@@ -5,3 +5,11 @@ views = Blueprint(__name__,"home")
 @views.route("/home")
 def home():
     return render_template("index.html")
+
+@views.route("/projects")
+def all_projects():
+    return render_template("projects.html")
+
+@views.route("/guppi")
+def guppi():
+    return render_template("guppi.html")
