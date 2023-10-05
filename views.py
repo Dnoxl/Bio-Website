d3ca5012f40082fb7eca5a1a6578eb01a3ebd1ac
Dnoxl <ifbimgamer@gmail.com>
2023-10-05 21:29:43 +0200

    integrated about into home

--- views.py
+++ views.py
@@ -5,7 +5,3 @@ views = Blueprint(__name__,"home")
 @views.route("/home")
 def home():
     return render_template("index.html")
-
-@views.route("/about")
-def about():
-    return render_template("about.html")
