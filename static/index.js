496543c8077da6643dcdce9c6094fa0c3807f98a
Dnoxl <ifbimgamer@gmail.com>
2023-10-07 19:54:42 +0200

    Added anim to embed

---
+++ static/index.js
@@ -0,0 +1,14 @@
+function loadIframe() {
+    var iframe = document.getElementById('dcserver-embed');
+    if (window.innerWidth < 480) {
+        iframe.style.visibility = "hidden";
+    } else {
+        iframe.style.visibility = "visible";
+        iframe.src = "https://discord.com/widget?id=1109530644578582590&theme=dark";
+        if (!iframe.classList.contains("open")) {
+            iframe.classList.add("open");
+        }
+    }
+}
+window.addEventListener('load', loadIframe);
+window.addEventListener('resize', loadIframe);
